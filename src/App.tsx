8c22623005ffaa2dea080545a3570b470e2d7945
BobbyKJH <rlatnguscjsw@gmail.com>
2023-03-02 19:06:24 +0900

    Feat: App common component Header
    1) 내용
    
    - Header component 연결
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+/* React-Router-Dom */
+import { Route, Routes } from "react-router-dom";
+/* Page */
+import MainPage from "./page/MainPage";
+/* Component */
+import Header from "./components/common/Header";
+
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
