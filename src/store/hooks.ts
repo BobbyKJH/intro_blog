91f4bf4ce9b7d331a3a351a1d773282078a01551
bobby <rlatnguscjsw@gmail.com>
2023-06-14 00:01:20 +0900

    Feat : redux
    - redux-toolkit 셋팅

---
+++ src/store/hooks.ts
@@ -0,0 +1,8 @@
+/** redux */
+import { useDispatch, useSelector } from 'react-redux'
+/** type */
+import type { TypedUseSelectorHook } from 'react-redux'
+import type { RootState, AppDispatch } from './store'
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
