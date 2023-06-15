8d7673ac448a456aefe342a6a1864a996ba25cf1
bobby <rlatnguscjsw@gmail.com>
2023-06-16 00:18:20 +0900

    Style : Header 반응형 수정
    - 600px -> 900px 변경

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+/** React */
+import React from "react";
 /** redux */
 import { useAppDispatch } from "@/store/hooks";
 import { setSliceState } from "@/store/slice/sidebarSlice";
@@ -12,16 +13,17 @@ import MenuIcon from '@mui/icons-material/Menu';
 const Header = () => {
   const dispatch = useAppDispatch();
 
-  const toggle = () => {
+  /** SideBar 버튼 */
+  const handleLinkChange = (event: React.MouseEvent<HTMLButtonElement>) => {
     dispatch(setSliceState());
-  }
+  };
 
   return (
     <AppBar
         position="relative"
         sx={{
-          width: { sm: `calc(100% - ${240}px)` },
-          ml: { sm: `${240}px` },
+          width: { md: `calc(100% - ${240}px)` },
+          ml: { md: `${240}px` },
         }}
       >
         <Toolbar>
@@ -29,13 +31,13 @@ const Header = () => {
             color="inherit"
             aria-label="open drawer"
             edge="start"
-            onClick={toggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            onClick={handleLinkChange}
+            sx={{ mr: 2, display: { md: 'none' } }}
           >
             <MenuIcon />
           </IconButton>
 
-          <Typography variant="h6" noWrap component="div" sx={{ justifyContent:"right" }}>
+          <Typography variant="h6" noWrap component="div" sx={{ justifyContent: "right" }}>
             BOBBY TLI BLOG
           </Typography>
 
