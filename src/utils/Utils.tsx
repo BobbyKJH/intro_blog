3fc90176ffc9bd796c4726d4bf155f61c3622afe
bobby <rlatnguscjsw@gmail.com>
2023-06-18 22:09:29 +0900

    Refactor : Utils pathName
    - 게시판 pathname 세분화로 인한 pathname에  따른 function 변경

--- src/utils/Utils.tsx
+++ src/utils/Utils.tsx
@@ -1,13 +1,13 @@
 "use client"
-
 import { usePathname } from "next/navigation";
 
+/** path에 따른 style 변경을 위한 utils 함수 */
 export const pathName = ( pathname: string ) => {
  const path = usePathname();
 
- if(pathname === "/"){
-  return path === "/"
- }
-
+ if(pathname === "/") return path === "/"
+ 
+ if(pathname === "/notice") return path === "/notice"
+ 
  return path.includes(pathname);
 }
