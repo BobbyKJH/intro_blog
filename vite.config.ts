41686b011fd49635ae89e78526e3faa2be4a1726
bobby <rlatnguscjsw@gmail.com>
2023-07-30 12:19:58 +0900

    Feat : 절대 경로
    - 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      /** path */
+      { find: "@", replacement: "/src" },
+      { find: "@type", replacement: "/src/type" },
+      { find: "@page", replacement: "/src/page" },
+      { find: "@style", replacement: "/src/style" },
+      { find: "@utils", replacement: "/src/utils" },
+      { find: "@components", replacement: "/src/components" },
+    ],
+  },
 })
