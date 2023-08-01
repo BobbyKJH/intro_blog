debf8a625b6ed4ba8b3d4b32292619832e56ef7f
bobby <rlatnguscjsw@gmail.com>
2023-08-01 23:44:33 +0900

    Feat : Router
    - Router HomePage
    - import 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+/** React */
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+/** Pate */
+import HomePage from "@page/HomePage.tsx";
+/** Component */
+import SideBar from "@components/common/SideBar.tsx";
+
 
 const App = () => {
-
   return (
-    <>
-      App
-    </>
-  )
-}
+    <BrowserRouter>
+      <SideBar/>
 
-export default App
+      <Routes>
+        <Route path={"/"} element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+/** React */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+/** Component */
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
